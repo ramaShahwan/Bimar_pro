25a9b568c659dd7232501c75ffd4fddc7948f425
ramaShahwan <rama692000@gmail.com>
2024-10-12 16:16:34 +0300

    edit

--- resources/views/admin/addcourse_enrollments.blade.php
+++ resources/views/admin/addcourse_enrollments.blade.php
@@ -38,14 +38,20 @@ h4{
                         <h4>تسجيل جديد</h4>
 
                             <div class="input-groupp">
-                            <select name="bimar_training_year_id" id="bimar_training_year_id">
+                            <select name="bimar_training_year_id" id="bimar_training_year_id" class="@error('bimar_training_year_id') is-invalid @enderror">
                          <option>اختر السنة التدريبية</option>
                              @foreach ($years as $year)
                                <option value="{{ $year->id }}">{{ $year->tr_year_name }}</option>
                              @endforeach
                         </select>
+                        @error('bimar_training_year_id')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
 
                             </div>
+
                             <div class="input-groupp input-groupp-icon">
     <select name="bimar_training_program_id" id="bimar_training_program_id" class="@error('bimar_training_program_id') is-invalid @enderror" aria-label="Default select example">
         <option selected>اختر البرنامج التدريبي</option>
@@ -53,72 +59,148 @@ h4{
             <option value="{{ $program->id }}">{{ $program->tr_program_name_ar }}</option>
         @endforeach
     </select>
+    @error('bimar_training_program_id')
+        <span class="invalid-feedback" role="alert">
+            <strong>{{ $message }}</strong>
+        </span>
+    @enderror
 </div>
 
 <div class="input-groupp input-groupp-icon">
     <select id="bimar_training_course_id" name="bimar_training_course_id" class="form-control @error('bimar_training_course_id') is-invalid @enderror">
         <option value="">-- اختر الكورس التدريبي --</option>
     </select>
+    @error('bimar_training_course_id')
+        <span class="invalid-feedback" role="alert">
+            <strong>{{ $message }}</strong>
+        </span>
+    @enderror
 </div>
 
                         <div class="input-groupp input-groupp-icon">
                             <div class="input-icon"><i class="fa-sharp fa-solid fa-calendar-week"></i></div>
-                          <input type="number" placeholder="رقم(ترتيب) الدورة التدريبية" name="tr_course_enrol_arrangement"/>
+                          <input type="number" placeholder="رقم(ترتيب) الدورة التدريبية" name="tr_course_enrol_arrangement" class="@error('tr_course_enrol_arrangement') is-invalid @enderror"/>
+                          @error('tr_course_enrol_arrangement')
+                          <span class="invalid-feedback" role="alert">
+                              <strong>{{ $message }}</strong>
+                          </span>
+                      @enderror
+                        </div>
 
-                        </div>
                         <div class="input-groupp input-groupp-icon">
-                          <input type="number" placeholder="نسبة الحسم على الدورة" name="tr_course_enrol_discount"/>
+                          <input type="number" placeholder="نسبة الحسم على الدورة" name="tr_course_enrol_discount" class="@error('tr_course_enrol_discount') is-invalid @enderror"/>
                           <div class="input-icon"><i class="fa-sharp fa-solid fa-calendar-week"></i></div>
+                          @error('tr_course_enrol_discount')
+                          <span class="invalid-feedback" role="alert">
+                              <strong>{{ $message }}</strong>
+                          </span>
+                      @enderror
                         </div>
+
                         <div class="input-groupp input-groupp-icon">
-                            <input type="text" placeholder="الوصف" name="tr_course_enrol_desc"/>
+                            <input type="text" placeholder="الوصف" name="tr_course_enrol_desc" class="@error('tr_course_enrol_desc') is-invalid @enderror"/>
                             <div class="input-icon"><i class="fa-sharp fa-solid fa-calendar-week"></i></div>
+                            @error('tr_course_enrol_desc')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
                           </div>
 
                         <div class="input-groupp input-groupp-icon">
                         <h4>تاريخ بداية التسجيل </h4>
-                          <input type="date" placeholder="تاريخ بداية التسجيل" style="padding-bottom: 0;" name="tr_course_enrol_reg_start_date"/>
+                          <input type="date" placeholder="تاريخ بداية التسجيل" style="padding-bottom: 0;" name="tr_course_enrol_reg_start_date" class="@error('tr_course_enrol_reg_start_date') is-invalid @enderror"/>
                           <div class="input-icon"><i class="fa-solid fa-calendar-days"></i></div>
+                          @error('tr_course_enrol_reg_start_date')
+                          <span class="invalid-feedback" role="alert">
+                              <strong>{{ $message }}</strong>
+                          </span>
+                      @enderror
                         </div>
+
                         <div class="input-groupp input-groupp-icon">
                         <h4>تاريخ نهاية التسجيل </h4>
-                            <input type="date" placeholder="تاريخ نهاية التسجيل" style="padding-bottom: 0;" name="tr_course_enrol_reg_end_date"/>
+                            <input type="date" placeholder="تاريخ نهاية التسجيل" style="padding-bottom: 0;" name="tr_course_enrol_reg_end_date" class="@error('tr_course_enrol_reg_end_date') is-invalid @enderror"/>
                             <div class="input-icon"><i class="fa-solid fa-calendar-days"></i></div>
+                            @error('tr_course_enrol_reg_end_date')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
                           </div>
                           <div class="input-groupp input-groupp-icon">
                           <h4>تاريخ بداية الجلسات </h4>
-                            <input type="date" placeholder="تاريخ بداية الجلسات" style="padding-bottom: 0;" name="tr_course_enrol_session_start_date"/>
+                            <input type="date" placeholder="تاريخ بداية الجلسات" style="padding-bottom: 0;" name="tr_course_enrol_session_start_date" class="@error('tr_course_enrol_session_start_date') is-invalid @enderror"/>
                             <div class="input-icon"><i class="fa-solid fa-calendar-days"></i></div>
+                            @error('tr_course_enrol_session_start_date')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
                           </div>
+
                           <div class="input-groupp input-groupp-icon">
                           <h4>تاريخ نهاية الجلسات </h4>
-                            <input type="date" placeholder="تاريخ نهاية الجلسات" style="padding-bottom: 0;" name="tr_course_enrol_session_end_date"/>
+                            <input type="date" placeholder="تاريخ نهاية الجلسات" style="padding-bottom: 0;" name="tr_course_enrol_session_end_date" class="@error('tr_course_enrol_session_end_date') is-invalid @enderror"/>
                             <div class="input-icon"><i class="fa-solid fa-calendar-days"></i></div>
+                            @error('tr_course_enrol_session_end_date')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
                           </div>
+
                           <div class="input-groupp input-groupp-icon">
-                            <input type="number" placeholder="علامة المحصلة النهائية" name="tr_course_enrol_mark"/>
+                            <input type="number" placeholder="علامة المحصلة النهائية" name="tr_course_enrol_mark"  class="@error('tr_course_enrol_mark') is-invalid @enderror"/>
                             <div class="input-icon"><i class="fa-sharp fa-solid fa-calendar-week"></i></div>
+                            @error('tr_course_enrol_mark')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
                           </div>
+
                           <div class="input-groupp input-groupp-icon">
-                            <input type="text" placeholder="علامة الامتحان النهائي " name="tr_course_enrol_oralmark"/>
+                            <input type="text" placeholder="علامة الامتحان النهائي " name="tr_course_enrol_oralmark"  class="@error('tr_course_enrol_oralmark') is-invalid @enderror"/>
                             <div class="input-icon"><i class="fa-sharp fa-solid fa-calendar-week"></i></div>
+                            @error('tr_course_enrol_oralmark')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
                           </div>
+
                           <div class="input-groupp input-groupp-icon">
-                          <input type="text" placeholder="علامة الشفهي  " name="tr_course_enrol_finalmark"/>
+                          <input type="text" placeholder="علامة الشفهي  " name="tr_course_enrol_finalmark"  class="@error('tr_course_enrol_finalmark') is-invalid @enderror"/>
                           <div class="input-icon"><i class="fa-sharp fa-solid fa-calendar-week"></i></div>
+                          @error('tr_course_enrol_finalmark')
+                          <span class="invalid-feedback" role="alert">
+                              <strong>{{ $message }}</strong>
+                          </span>
+                      @enderror
                         </div>
+
                         <div class="input-groupp input-groupp-icon">
-                            <input type="text" placeholder="رسوم التسجيل" name="tr_course_enrol_price"/>
+                            <input type="text" placeholder="رسوم التسجيل" name="tr_course_enrol_price"  class="@error('tr_course_enrol_price') is-invalid @enderror"/>
                             <div class="input-icon"><i class="fa-sharp fa-solid fa-calendar-week"></i></div>
+                            @error('tr_course_enrol_price')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
                           </div>
                         <div class="input-groupp input-groupp-icon">
-                        <select name="bimar_training_type_id" id="bimar_training_type_id">
+                        <select name="bimar_training_type_id" id="bimar_training_type_id"  class="@error('bimar_training_type_id') is-invalid @enderror">
                          <option>اختر  نوع التدريب</option>
                              @foreach ($types as $type)
                                <option value="{{ $type->id }}">{{ $type->tr_type_name_ar }}</option>
                              @endforeach
                         </select>
-
+                        @error('bimar_training_type_id')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
 
                         </div>
                       </div>
